ade99c68631e4b322577d9c5e72c78f854ff4315
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2024-02-23 15:58:14 +0200

    Splash Screen added

--- City-Pulse-Android/app/src/main/java/com/andreeailie/citypulse/MainActivity.kt
+++ City-Pulse-Android/app/src/main/java/com/andreeailie/citypulse/MainActivity.kt
@@ -3,6 +3,7 @@ package com.andreeailie.citypulse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +11,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.andreeailie.citypulse.ui.theme.CityPulseTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val splashScreen = installSplashScreen()
+
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            lifecycleScope.launch {
+                viewModel.isLoading.collect { isLoading ->
+                    if (!isLoading) {
+                        splashScreenViewProvider.view.animate()
+                            .alpha(0f)
+                            .setDuration(200L)
+                            .withEndAction {
+                                splashScreenViewProvider.remove()
+                            }
+                    }
+                }
+            }
+        }
+
         setContent {
             CityPulseTheme {
                 // A surface container using the 'background' color from the theme
